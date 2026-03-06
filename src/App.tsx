f6c9793ea99b192c10924e07f4f3bf32d9d5f40b
Ojay97-hub <owenjames97@outlook.com>
2026-03-06 16:59:47 +0000

    fix: integrated a scroll to top functionality so pages start at the top on all screen sizes on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Home } from './pages/Home';
@@ -13,6 +14,7 @@ import { AdminLogin } from './pages/AdminLogin';
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-1">
